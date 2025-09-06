67c5b4d8df4bb615d4b9b24537359e3c70377fe9
Rajib Chakraborty <rajibchakraborty1995@gmail.com>
2025-09-06 20:27:31 +0530

    Updated calculator styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator-app';
+  numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  operators = ['+', '-', '*', '/'];
+
+  expressionArray = signal<string[]>([]);
+  result = signal<number | string>(0);
+
+  addToExpression(value: string) {
+    this.expressionArray.update(arr => [...arr, value]);
+  }
+
+  calculateExpression() {
+    try {
+      const expression = this.expressionArray().join('');
+      const sanitizedExpression = expression.replace(/[^-()\d/*+.]/g, '');
+      const evalResult = new Function('return ' + sanitizedExpression)();
+      this.result.set(evalResult);
+    } catch (error) {
+      this.result.set('Invalid expression');
+    }
+  }
+
+  clearExpression() {
+    this.expressionArray.set([]);
+    this.result.set(0);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CommonModule,MatIconModule,MatDividerModule,MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
